067ed6271bc277c4f9b6d07fd6b476dfc588e21c
Corrre2 <correacarol1996@gmail.com>
2024-04-25 08:45:56 -0300

    button

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import { View, TextInput, Text } from "react-native";
+import React, {useState} from "react";
+import { View, TextInput, Text,Button} from "react-native";
 export default function Form(){
+
+    const[height, setHeight] = useState(null)
+    const[weight, setWeight] = useState(null)
+    const[messageImc, setMessageimc] = useState("prenche ai obeso")
+    const [imc, setImc] = useState(null)
+    const[textButton, setTextButton] = useState("calcular obesidade")
     return (
         <View>
 
@@ -14,6 +20,10 @@ export default function Form(){
             <TextInput
         placeholder="Ex. 80.365"
         keyboardType="numeric"/>
+        <Button
+  onPress={()=> validationImc()}
+  title={textButton}
+/>
           </View>
 
           </View>

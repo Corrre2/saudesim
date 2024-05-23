8b2042b6bce244f8af6c0507c359141669fb9f02
Corre2 <correacarol1996@gmail.com>
2024-05-23 08:47:59 -0300

    SaudeSIM

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,23 +1,30 @@
 import React, {useState} from "react";
 import { View, TextInput, Text,Button} from "react-native";
+import ResultImc from "./ResultImc";
+
 export default function Form(){
 
     const[height, setHeight] = useState(null)
     const[weight, setWeight] = useState(null)
     const[messageImc, setMessageimc] = useState("prenche ai obeso")
     const [imc, setImc] = useState(null)
-    const[textButton, setTextButton] = useState("calcular obesidade")
+    const[textButton, setTextButton] = useState("calcular IMC")
     return (
         <View>
 
         <View>
             <Text>Altura</Text>
             <TextInput
+            onChangeText={setHeight}
+            value={height}
         placeholder="Ex. 1.75"
         keyboardType="numeric"/>
 
+
         <Text>Peso</Text>
             <TextInput
+              onChangeText={setWeight}
+              value={weight}
         placeholder="Ex. 80.365"
         keyboardType="numeric"/>
         <Button
@@ -25,7 +32,8 @@ export default function Form(){
   title={textButton}
 />
           </View>
-
+          <ResultImc messageResulImc={messageImc}
+          ResultImc={imc}/>
           </View>
     );
 }
